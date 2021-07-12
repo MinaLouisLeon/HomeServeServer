9b633f1d7953737e7440b7c98d5ad487cde81d55
Mina Louis <minalouis@Minas-MacBook-Pro.local>
2021-07-12 19:53:18 +0200

    starting of server

---
+++ Server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const knex = require('knex');
+const cors = require('cors');
+const { json } = require('body-parser');
+var mqtt = require('mqtt');
+const app = express();
+app.use(cors());
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : '3694',
+      database : 'borg_el_arab' 
+    }
+});
+
+
+app.use(bodyparser.json())
+
+app.get('/lights',(req,res) => {
+    db.select('*').from('lights').then((data) => {
+        res.json(data)
+    })
+})
+
+app.listen(3000);
+
